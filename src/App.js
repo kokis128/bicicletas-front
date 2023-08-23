0f176adca31159eafc98540200575b6dbe99696e
unknown <Jorge>
2023-08-23 16:33:48 -0300

    my secon commits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 
 import './App.css';
+import {React} from 'react';
+import {BrouserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/home/home'
+import Login from './pages/login/login';
+import Register from './pages/registro/register';
+import NotFound from './pages/notFound/notFound';
+
+function AppWrapper(){
+  return(
+    <Routes>
+
+      <Route path="/" element={<Home/>} />
+      <Route path="/home" element={<Home/>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/404" element={<notFound/>} />
+      <Route path="*" element={NotFound} />
+
+    </Routes>
+  )
+}
+
+
+
+
+
 
 function App() {
   return (
---
+++ src/pages/login/login.js
@@ -0,0 +1,16 @@
+import {React} from 'react';
+import "./login.css";
+
+function Login(){
+
+
+    return(
+        <div className="main">
+
+        Login
+
+        </div>
+    );
+}
+
+export default Login;
