82670d16ccd0793c633fd2d75820c3d23428fa5f
unknown <Jorge>
2023-08-23 18:32:13 -0300

    add form

--- src/pages/perfil/perfil.js
+++ src/pages/perfil/perfil.js
@@ -1,5 +1,6 @@
 import {React} from 'react';
 import "./perfil.css";
+import NavBar from '../../components/navbar/navbar';
 
 
 function Perfil(){
@@ -8,7 +9,23 @@ function Perfil(){
     return(
         <div className="main">
 
-      
+      <NavBar />
+
+      <form onSubmit={console.log("Contraseña actualizada")}></form>
+      <div className='div-form'>
+        <label>Email</label>
+        <div>email@gamail.com</div>
+
+
+      </div>
+      <div className='div-form'>
+
+      <label>Email</label>
+
+      <input className='input-form'/>
+
+
+      </div>
 
         </div>
     );
