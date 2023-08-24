9dfe53134e02541ecf1aef753027401600448f18
unknown <Jorge>
2023-08-24 17:41:34 -0300

    styles

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,5 +1,8 @@
 import {React} from 'react';
 import "./navbar.css";
+import {Link} from 'react-router-dom';
+
+
 
 function NavBar(){
 
@@ -8,13 +11,13 @@ function NavBar(){
         <div className="navbar-size">
             <div className='navbar-left'>
 
-            <h3 className='nav-h3'>bicicletas</h3>
-            <h3 className='nav-h3'>Alquileres</h3>
+          <Link to="/#">  <h3 className='nav-h3'>bicicletas</h3></Link>
+          <Link to="/#"> <h3 className='nav-h3'>Alquileres</h3></Link>
             </div>
             <div className='navbar-center'></div>
             <div className='navbar-right'>
-            <h3 className='nav-h3'>Perfil</h3>
-            <h3 className='nav-h3'>Salir</h3>
+            <Link to="/perfil"><h3 className='nav-h3'>Perfil</h3></Link>
+            <Link to="/#"><h3 className='nav-h3'>Salir</h3></Link>
             </div>
             
            
--- src/pages/perfil/perfil.js
+++ src/pages/perfil/perfil.js
@@ -7,54 +7,64 @@ function Perfil(){
 
 
     return(
-        <div className="main">
+      <>
+      <NavBar />
+        <div>
+
+          <div className="main">
+
+          <form className='form-size' onSubmit={console.log("Contraseña actualizada")}>
+      <div className='div-form'>
+        <label>Email</label>
+        <h3 className='h3-form'>email@gamail.com</h3>
+      </div>
+
+
+
+      <div className='div-form'>
+      <label className='label-form'>Nombre</label>
+      <h3 className='h3-form'>Juan Perez</h3>
+      </div>
+
+      <div className='div-form'>
+     
+      <input className='input-form'  type="password" placeholder="ingrese contraseña acual"/>
+      </div>
+
+      <div className='div-form'>
+     
+      <input className='input-form'  type="password" placeholder="ingrese nueva contraseña"/>
+      </div>
+      
+      <div className='div-form'>
+      
+      <input className='input-form'  type="password" placeholder="confirmar nueva contraseña"/>
+      </div>
+
+     
+      <button className='button-form' type='submit'>
+        Guardar
+      </button>
+
+      </form>
+
+
+
+
+
+
+          </div>
+
+      
 
      
 
-      <form onSubmit={console.log("Contraseña actualizada")}>
 
       
-      <div className='div-form'>
-        <label>Email</label>
-        <div>email@gamail.com</div>
-        </div>
-
-
-      <div className='div-form'>
-      <label>Nombre</label>
-      <div>Juan Perez</div>
-      </div>
-
-      <div className='div-form'>
-      <label>Nombre</label>
-      <input type='password' placeholder='Ingrese contraseña Actual' /> 
-      </div>
-
-      <div className='div-form'>
-      <label>Nombre</label>
-      <input type='password' placeholder='Ingrese mueva contraseña' /> 
-      </div>
-
-      <div className='div-form'>
-      <label>Nombre</label>
-      <input type='password' placeholder='Confirmar mueva contraseña' /> 
-
-      </div>
-
-      <button className='button-form' type='submit'>
-
-        guabdar 
-        
-      </button>
-
-      <div className='div-form'>
-      <label>Nombre</label>
-      <div>Juan Perez</div>
-      </div>
-      
-      </form>
 
         </div>
+
+        </>
     );
 }
 
